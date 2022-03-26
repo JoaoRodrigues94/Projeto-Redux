c78bc52e8b9fbbbb00b9333ecf15ce79bdec0684
JoaoRodrigues94 <joaopaulo.fr.1994@gmail.com>
2022-03-26 15:44:38 +0000

    Introdução do redux nos arquivos

--- redux/src/Components/Sorteio.jsx
+++ redux/src/Components/Sorteio.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Card from "./Card";
 
-export default props => {
-    // const {min, max} = props
-    // const aleatorio = parseInt(Math.random() * (max-min)) + min
+import {connect} from "react-redux";
+
+function Sorteio(props) {
+    const {min, max} = props
+    const aleatorio = parseInt(Math.random() * (max-min)) + min
     return(
         <Card title="Sorteio dos Números" purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    {/* <span>{aleatorio}</span> */}
-                    <span>{13}</span>
+                    <span>{aleatorio}</span>
                 </span>
             </div>  
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio)
